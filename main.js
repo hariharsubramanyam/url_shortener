c878cf9815520b1066a63ce55bd8b126fce75874
Harihar Subramanyam <hahasubbu@gmail.com>
2016-09-15 00:07:02 -0400

    Comments to main.js

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 var express = require("express");
-var app = express();
-
 var bodyParser = require('body-parser')
 var path = require("path");
 var golinks = require("./golinks.js");
 
+var app = express();
 app.use(bodyParser.urlencoded({ extended: false }))
 
+// Create mapping page.
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname+'/index.html'));
 });
 
+// Expand shortened URL.
 app.get("/links/:short", function(req, res) {
   var url = golinks.expand(req.params.short);
   if (url) {
@@ -20,12 +21,11 @@ app.get("/links/:short", function(req, res) {
   }
 });
 
+// Shorten URL.
 app.post("/links", function(req, res) {
-  var url = req.body.url;
   var short = req.body.short;
   short = short.length !== 0 ? short : undefined;
-
-  short = golinks.add(url, short);
+  short = golinks.add(req.body.url, short);
   golinks.save(function() {
     res.send(short);
   });
